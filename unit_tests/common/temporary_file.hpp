9a573e0cbfeeba3aeb5e1ae095e9166f78bd773f
Dennis Luxen <info@project-osrm.org>
2022-12-10 21:48:23 +0100

    Port boost::filesystem::unique_path()

--- unit_tests/common/temporary_file.hpp
+++ unit_tests/common/temporary_file.hpp
@@ -2,16 +2,38 @@
 #define UNIT_TESTS_TEMPORARY_FILE_HPP
 
 #include <filesystem>
+#include <random>
 #include <string>
 
 struct TemporaryFile
 {
-    TemporaryFile() : path(std::filesystem::unique_path()) {}
+    TemporaryFile() : path(std::filesystem::temp_directory_path() /= random_string(8)) {}
     TemporaryFile(const std::string &path) : path(path) {}
 
     ~TemporaryFile() { std::filesystem::remove(path); }
 
     std::filesystem::path path;
+
+  private:
+    std::string random_string(std::string::size_type length)
+    {
+        static auto &chrs = "0123456789"
+                            "abcdefghijklmnopqrstuvwxyz"
+                            "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        thread_local static std::mt19937 rg{std::random_device{}()};
+        thread_local static std::uniform_int_distribution<std::string::size_type> pick(
+            0, sizeof(chrs) - 2);
+
+        std::string s;
+
+        s.reserve(length);
+
+        while (length--)
+            s += chrs[pick(rg)];
+
+        return s;
+    }
 };
 
 #endif // UNIT_TESTS_TEMPORARY_FILE_HPP
